4432abef1af2b6bc7ec5a9037af5decd51d49aa2
pryahin <pryahin.v@gmail.com>
2017-09-15 14:03:58 +0300

    Load img from file

--- script.js
+++ script.js
@@ -1,81 +1,79 @@
 let canvas = document.getElementById('lab01');
 const ctx = canvas.getContext('2d');
 
-ctx.fillStyle = "#ff0000";
-ctx.fillRect(10, 10, 1000, 1000);
+const PARAMS = {
+    width: 200,
+    height: 200,
+    paintSize: 10
+};
+
 ctx.fillStyle = "#000000";
 
-canvas.addEventListener('click', function(event){
+canvas.addEventListener('click', function (event) {
     ctx.fillStyle = "#000000";
 
-    ctx.fillRect(event.offsetX,event.offsetY,10,10);
+    ctx.fillRect(event.offsetX, event.offsetY, PARAMS.paintSize, PARAMS.paintSize);
 });
 
-function saveTextAsFile()
-{
-    var textToWrite = document.getElementById("inputTextToSave").value;
-    var textFileAsBlob = new Blob([textToWrite], {type:'text/plain'});
+function saveTextAsFile() {
 
-    // let x=0, y=0;
-    // while (x<100 && y<100)
-    // {
-    //     ctx.fillRect(x+10, y+10, 10*(1+x), 10*(1+y));
-    //     x++;
-    //     if (x > 10) {
-    //         x = 0;
-    //         y++;
-    //     }
-    // }
-    var fileNameToSaveAs ="123";// document.getElementById("inputFileNameToSaveAs").value;
+    const img = ctx.getImageData(10, 10, PARAMS.width, PARAMS.height).data;
+    let text = '';
 
-    var downloadLink = document.createElement("a");
-    downloadLink.download = fileNameToSaveAs;
-    downloadLink.innerHTML = "Download File";
-    if (window.webkitURL != null)
-    {
-        // Chrome allows the link to be clicked
-        // without actually adding it to the DOM.
-        downloadLink.href = window.webkitURL.createObjectURL(textFileAsBlob);
+    for (let i = 3; i < img.length; i += 4) {
+        console.log(img[i]);
+        text += img[i] ? '1' : '0';
     }
-    else
-    {
-        // Firefox requires the link to be added to the DOM
-        // before it can be clicked.
-        downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+
+    const textFile = new Blob([text], {type: 'text/plain'});
+    const fileNameToSaveAs = "123";// document.getElementById("inputFileNameToSaveAs").value;
+
+    const downloadLink = document.createElement("a");
+    downloadLink.download = fileNameToSaveAs;
+    downloadLink.innerHTML = 'Download File';
+
+    if (window.webkitURL !== null) {
+        downloadLink.href = window.webkitURL.createObjectURL(textFile);
+    }
+    else {
+        downloadLink.href = window.URL.createObjectURL(textFile);
         downloadLink.onclick = destroyClickedElement;
-        downloadLink.style.display = "none";
+        downloadLink.style.display = 'none';
+
         document.body.appendChild(downloadLink);
     }
 
     downloadLink.click();
 }
 
-function destroyClickedElement(event)
-{
+function destroyClickedElement(event) {
     document.body.removeChild(event.target);
 }
 
 
-document.getElementById('loadbutton').addEventListener('click', function() {
+document.getElementById('loadbutton').addEventListener('click', function () {
     var fileToLoad = document.getElementById("fileToLoad").files[0];
 
     var fileReader = new FileReader();
 
-    ctx.fillStyle = "#ff0000";
-    ctx.fillRect(10, 10, 1000, 1000);
     ctx.fillStyle = "#000000";
 
-    fileReader.onload = function(fileLoadedEvent)
-    {
-        let x=0,y=0;
+    fileReader.onload = function (fileLoadedEvent) {
+        let x = 0, y = 0;
         var textFromFileLoaded = fileLoadedEvent.target.result;
-        for (char in textFromFileLoaded){
-            if (textFromFileLoaded[char]=='1') {
-                ctx.fillRect(10*(1+x), 10*(y+1), 10, 10);
+        for (char in textFromFileLoaded) {
+            if (textFromFileLoaded[char] === '1') {
+                ctx.fillRect(10 + x, 10 + y, 1, 1);
             }
-            else if (textFromFileLoaded[char]=="\n"){
-                x=0;
+            else if (textFromFileLoaded[char] === '\n') {
+                x = 0;
                 y++;
+                continue;
+            }
+            if (x === PARAMS.width-1) {
+                x = 0;
+                y++;
+                continue;
             }
 
             x++;
